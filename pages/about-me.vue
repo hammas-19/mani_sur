9db7dbc1d865a49b1006f8c08b419d7dba53f3a8
hammas-19 <hammasmasood@gmail.com>
2023-11-21 16:37:49 +0500

    style: Updated code Snippets section

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -161,7 +161,7 @@
             <h3 class="text-white lg:text-menu-text mb-4 text-sm">// Code snippet showcase:</h3>
 
             <div class="flex flex-col overflow-scroll">
-              <GistSnippet data-aos="fade-down" v-for="(gist, key) in config.public.dev.gists" :key="key" :id="gist" />
+              <GistSnippet data-aos="fade-down" />
             </div>
           </div>
 
